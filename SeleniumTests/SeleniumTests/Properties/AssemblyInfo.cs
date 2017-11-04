570df98456a4ece6db9d52444a159e5a6215f206
Jeffrey Alvarado <jefalva10296@gmail.com>
2017-11-04 15:31:44 -0600

    Initial commit

---
+++ SeleniumTests/SeleniumTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("SeleniumTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fb7750dd-4cc3-471a-9bb9-eca511c4bfc8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumTests/SeleniumTests/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestAutenticacion()
+        {
+            String url = "http://localhost:50272/Autenticacion.aspx";
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Id("textUsername")).SendKeys("jefalva@live.com");
+            driver.FindElement(By.Id("textPassword")).SendKeys("102961");
+            driver.FindElement(By.Id("Button1")).Click();
+            driver.Close();
+            driver.Dispose();
+        }
+    }
+}
